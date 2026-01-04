bdff5577fc09a455434c474a9ebf070d86933150
chouunb <sbt030309@gmail.com>
2026-01-04 20:47:08 +0300

    feat(products): Добавила функционал получения деталей товара: - шаблон с деталями товара - маршрут и обработчик для этого маршрута

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.get_products_list),
+    path("products/", views.get_product_list, name='product_list'),
+    path("products/<int:product_id>/", views.get_product_detail, name='product_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Product
 
 
-def get_products_list(request):
+def get_product_list(request):
     products = Product.objects.all()
 
     return render(request, template_name='blog/product_list.html', context={'products': products})
 
+
+def get_post_detail(request, product_id):
+    return render(request, 'blog/product_detail.html', {"product": get_object_or_404(Product, id=product_id)})
