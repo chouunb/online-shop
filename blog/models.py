bafe719167d34eaf887aaff207dfb7ec0e083089
chouunb <sbt030309@gmail.com>
2026-01-04 17:36:53 +0300

    feat(posts): Создала модель товара и зарегистрировала в админке

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2,verbose_name="Цена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    class Meta:
+        verbose_name = "товар"
+        verbose_name_plural = "товары"
+        db_table = "shop_products"
+
+    def __str__(self):
+        return self.name
