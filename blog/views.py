248579eaffdb9e643a273827c3d8e564ec8a3321
chouunb <sbt030309@gmail.com>
2026-01-04 21:37:48 +0300

    refactor(blog): Наследование шаблонов: - добавила базовый шаблон - наследовала от базового шаблона остальные шаблоны

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,8 @@ from blog.models import Product
 def get_product_list(request):
     products = Product.objects.all()
 
-    return render(request, template_name='blog/product_list.html', context={'products': products})
+    return render(request, template_name='shop/product_list.html', context={'products': products})
 
 
-def get_post_detail(request, product_id):
-    return render(request, 'blog/product_detail.html', {"product": get_object_or_404(Product, id=product_id)})
+def get_product_detail(request, product_id):
+    return render(request, 'shop/product_detail.html', {"product": get_object_or_404(Product, id=product_id)})
