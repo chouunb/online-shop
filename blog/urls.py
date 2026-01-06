2694e8910a62e8bc327bf8ec7c29cefc51cd0259
chouunb <sbt030309@gmail.com>
2026-01-06 16:24:35 +0300

    feat(nav): Добавила навигационное меню

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("products/", views.get_product_list, name='product_list'),
     path("products/<int:product_id>/", views.get_product_detail, name='product_detail'),
-    path('products/add/', views.create_product),
+    path('products/add/', views.create_product, name='new_product'),
 ]
