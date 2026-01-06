36e33909cff05eb5740354d8e7b3f8e4edf273bc
chouunb <sbt030309@gmail.com>
2026-01-06 16:17:09 +0300

    feat(products): Добавила функционал для добавления товара для продажи (самый простой вариант)

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from blog.models import Product
 
@@ -11,3 +11,13 @@ def get_product_list(request):
 
 def get_product_detail(request, product_id):
     return render(request, 'shop/product_detail.html', {"product": get_object_or_404(Product, id=product_id)})
+
+
+def create_product(request):
+    if request.method == "GET":
+        return render(request, 'shop/product_add.html')
+
+    if request.method == "POST":
+        product = Product.objects.create(name=request.POST.get('name'), description=request.POST.get('description'), price=request.POST.get('price'))
+
+        return redirect('product_detail', product_id=product.id)
