63e423a78ff67a694d2b0665d5bd27f56c2659c1
chouunb <sbt030309@gmail.com>
2026-01-07 18:39:49 +0300

    feat(users/login): Реализовала функционал входа в аккаунт

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 
 def register_view(request):
@@ -8,10 +9,25 @@ def register_view(request):
 
         if form.is_valid():
             form.save()
-            return redirect('blog:product_list')
+            return redirect('users:login')
         else:
             return render(request, 'users/register.html', {'form': form})
 
     form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+
+            return redirect("blog:product_list")
+        else:
+            return render(request, 'users/login.html', {'form': form})
+
+    form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
