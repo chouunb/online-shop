9776a19bf06ff8ddfda9271a9db084c9124b3eb9
chouunb <sbt030309@gmail.com>
2026-01-07 13:28:07 +0300

    feat(update-product): Добавила ссылку на редактирование в деталях объявления

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("products/", views.get_product_list, name='product_list'),
     path("products/<int:product_id>/", views.get_product_detail, name='product_detail'),
     path('products/add/', views.create_product, name='new_product'),
-    path('products/<int:product_id>/edit/', views.update_product),
+    path('products/<int:product_id>/edit/', views.update_product, name="edit_product"),
 ]
