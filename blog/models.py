a723821b79249ce8c05a5ea78c95b7bfe125513b
chouunb <sbt030309@gmail.com>
2026-02-01 15:57:46 +0300

    feat(products/image): Установила pillow, добавила изображение для товара, добавила отображение изображения в деталях товара. Реализовала обработку медиафайлов

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название")
     description = models.TextField(blank=True, verbose_name="Описание")
     price = models.IntegerField(verbose_name="Цена")
+    image = models.ImageField(upload_to="product_images/", null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
 
--- config/settings.py
+++ config/settings.py
@@ -123,5 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
 
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+
 LOGIN_URL = 'users:login'
 DEFAULT_LOGIN_REDIRECT_URL = 'users:profile'
