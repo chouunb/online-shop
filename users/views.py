9d1fae43e81fb52c38a86a48c62091f2d2380b3c
chouunb <sbt030309@gmail.com>
2026-02-23 12:02:26 +0300

    refactor(users/views.py): Переписалa logout_view на CBV

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import logout, get_user_model
+from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from django.conf import settings
 
@@ -27,9 +27,8 @@ class CustomLoginView(LoginView):
         return next_url
 
 
-def logout_view(request):
-    logout(request)
-    return redirect("blog:product_list")
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('blog:product_list')
 
 
 def profile_view(request, username):
