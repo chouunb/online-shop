17b4766b6ac4803d80add950c229157176a750f9
chouunb <sbt030309@gmail.com>
2026-05-23 12:12:16 +0300

    feat(users): Кастомизировалa админку для модели CustomUser

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.safestring import mark_safe
 
 from users.models import CustomUser
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "get_avatar", "selected_theme", "is_staff")
+    list_filter = ("selected_theme", "is_staff", "is_active")
+    
+    # Добавление новых полей в формы редактирования
+    fieldsets = UserAdmin.fieldsets + (
+        ("Дополнительная информация", {
+            "fields": ("avatar", "selected_theme")
+        }),
+    )
+    
+    # Добавление новых полей в форму создания
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Дополнительная информация", {
+            "fields": ("email", "avatar", "selected_theme")
+        }),
+    )
+
+    # Метод для отображения миниатюры аватара
+    def get_avatar(self, obj):
+        if obj.avatar:
+            return mark_safe(f'<img src="{obj.avatar.url}" width="35" height="35" style="border-radius:50%; object-fit:cover;"/>')
+        return "———"
+    get_avatar.short_description = "Аватар"
